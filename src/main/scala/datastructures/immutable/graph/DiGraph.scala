22a2689a7ccf43d9e575d7982910f087ace001b5
Venkat Sudheer Reddy Aedama <sudheer.univ@gmail.com>
2019-07-14 17:52:55 -0700

    merge add/remove vertices for directed and undirected graphs and fix add/remove edge

--- src/main/scala/datastructures/immutable/graph/DiGraph.scala
+++ src/main/scala/datastructures/immutable/graph/DiGraph.scala
@@ -1,27 +1,14 @@
 package datastructures.immutable.graph
 
-class DiGraph[Vertex](store: Map[Vertex, List[Vertex]]) extends AdjacencyListGraph[Vertex](store) {
+class DiGraph[Vertex](store: Map[Vertex, List[Vertex]]) extends UndirectedGraph[Vertex](store) {
 
   // O(1) time and space
-  override def addVertex(vertex: Vertex): DiGraph[Vertex] =
-    if (!store.contains(vertex)) new DiGraph(store.updated(vertex, Nil))
-    else this
-
-  // O(|V| + |E|) time and space
-  override def removeVertex(vertex: Vertex): DiGraph[Vertex] =
-    if (!store.contains(vertex)) this
-    else new DiGraph((store - vertex).mapValues(_.filter(_ != vertex)))
-
-  // O(1) time and space
-  override def addEdge(vertex1: Vertex, vertex2: Vertex): DiGraph[Vertex] =
-    new DiGraph(store
-      .updated(vertex1, vertex2 +: store.getOrElse(vertex1, Nil))
-      .updated(vertex2, store.getOrElse(vertex2, Nil))
-    )
+  override def addEdge(v1: Vertex, v2: Vertex): DiGraph[Vertex] =
+    new DiGraph(store + (v1 -> (v2 +: store.getOrElse(v1, Nil))))
 
   // O(|E|) time and space
-  override def removeEdge(vertex1: Vertex, vertex2: Vertex): DiGraph[Vertex] =
-    if (store.contains(vertex1)) new DiGraph(store.updated(vertex1, store(vertex1).filter(_ != vertex2)))
+  override def removeEdge(v1: Vertex, v2: Vertex): DiGraph[Vertex] =
+    if (store.contains(v1)) new DiGraph(store.updated(v1, store(v1).filter(_ != v2)))
     else this
 
 }
--- src/main/scala/datastructures/immutable/graph/UndirectedGraph.scala
+++ src/main/scala/datastructures/immutable/graph/UndirectedGraph.scala
@@ -3,25 +3,27 @@ package datastructures.immutable.graph
 class UndirectedGraph[Vertex](store: Map[Vertex, List[Vertex]]) extends AdjacencyListGraph[Vertex](store) {
 
   // O(1) time and space
-  override def addVertex(vertex: Vertex): UndirectedGraph[Vertex] =
-    if (!store.contains(vertex)) new UndirectedGraph(store.updated(vertex, Nil))
+  override def addVertex(v: Vertex): AdjacencyListGraph[Vertex] =
+    if (!store.contains(v)) new UndirectedGraph(store.updated(v, Nil))
     else this
 
-  // O(|V| + 2*|E|) time and space
-  override def removeVertex(vertex: Vertex): UndirectedGraph[Vertex] =
-    if (!store.contains(vertex)) this
-    else new UndirectedGraph(store.filterKeys(_ == vertex).mapValues(_.filter(_ != vertex)))
+  // O(V) time and space
+  override def removeVertex(v: Vertex): AdjacencyListGraph[Vertex] =
+    if (!store.contains(v)) this
+    else new DiGraph((store - v).mapValues(_.filter(_ != v)))
 
   // O(1) time and space
-  override def addEdge(vertex1: Vertex, vertex2: Vertex): UndirectedGraph[Vertex] =
-    new UndirectedGraph(store
-      .updated(vertex1, vertex2 +: store.getOrElse(vertex1, Nil))
-      .updated(vertex2, vertex1 +: store.getOrElse(vertex2, Nil)))
+  override def addEdge(v1: Vertex, v2: Vertex): AdjacencyListGraph[Vertex] = {
+    val v1Neighbors = v2 +: store.getOrElse(v1, Nil)
+    val v2Neighbors = v1 +: store.getOrElse(v2, Nil)
+    new UndirectedGraph(store + (v1 -> v1Neighbors) + (v2 -> v2Neighbors))
+  }
 
   // O(2*|E|) time and space
-  override def removeEdge(vertex1: Vertex, vertex2: Vertex): UndirectedGraph[Vertex] =
-    new UndirectedGraph(store
-      .updated(vertex1, store.getOrElse(vertex1, Nil).filter(_ != vertex2))
-      .updated(vertex2, store.getOrElse(vertex2, Nil).filter(_ != vertex1)))
+  override def removeEdge(v1: Vertex, v2: Vertex): AdjacencyListGraph[Vertex] = {
+    val v1Neighbors = store.getOrElse(v1, Nil).filter(_ != v2)
+    val v2Neighbors = store.getOrElse(v2, Nil).filter(_ != v1)
+    new UndirectedGraph(store + (v1 -> v1Neighbors) + (v2 -> v2Neighbors))
+  }
 
 }
