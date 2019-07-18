fc5e87789c788d73915b55aab48f293ca2b91b20
Venkat Sudheer Reddy Aedama <sudheer.univ@gmail.com>
2019-07-17 19:38:38 -0700

    use ArrayBuffer instead of ListBuffer for better performance; improve comments

--- src/main/scala/algorithms/merge_intervals/MergeOverlappingIntervals.scala
+++ src/main/scala/algorithms/merge_intervals/MergeOverlappingIntervals.scala
@@ -1,26 +1,30 @@
 package algorithms.merge_intervals
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
 
-// Intuition:
-// When does an overlap occur?
-// If prevTo >= nextFrom
+// Intuition: When does an overlap occur?
+// Given two intervals (prevFrom, prevTo) and (nextFrom, nextTo)
+// an overlap occurs if nextFrom <= prevTo
 
-// We can have two kinds of overlapping scenarios:
-// 1. Full overlap (prevTo >= nextTo) => (prevFrom, prevTo)
+// There are two possible overlapping scenarios:
+
+// 1. Full overlap: nextFrom <= prevTo and nextTo <= prevTo
+// Merged = (prevFrom, prevTo)
 // [(1,7), (4,6)] => (1,7)
 
-// 2. Partial overlap => (prevFrom, nextTo)
+// 2. Partial overlap: nextTo > prevTo
+// Merged = (prevFrom, nextTo)
 // [(1,5), (3,7)] => (1,7)
 object MergeOverlappingIntervals {
 
-  def merge(sortedIntervals: Seq[(Int, Int)]): Seq[(Int, Int)] = sortedIntervals match {
-    case Nil => Nil
-    case hd :: tail =>
-      val result = new ListBuffer[(Int, Int)]
-      result += hd
+  def merge(intervals: Seq[(Int, Int)]): Seq[(Int, Int)] = {
+    if (intervals.isEmpty) Nil
+    else {
+      val sorted = intervals.sortBy(_._1)
+      val result = new ArrayBuffer[(Int, Int)]()
+      result += sorted.head
 
-      tail.foreach { case (nextFrom, nextTo) =>
+      sorted.tail.foreach { case (nextFrom, nextTo) =>
         val (prevFrom, prevTo) = result.last
 
         if (isOverlap(prevTo, nextFrom)) {
@@ -29,21 +33,15 @@ object MergeOverlappingIntervals {
             result += prevFrom -> nextTo
           }
         } else result += nextFrom -> nextTo
-
       }
-
-      result.toList
+      result
+    }
   }
 
-  private def isOverlap(prevTo: Int, nextFrom: Int): Boolean = prevTo >= nextFrom
+  private def isOverlap(prevTo: Int, nextFrom: Int): Boolean =
+    nextFrom <= prevTo
 
-  private def isFullOverlap(prevTo: Int, nextTo: Int): Boolean = prevTo >= nextTo
-
-}
-
-object MergeOverlappingIntervalsDemo extends App {
-
-
+  private def isFullOverlap(prevTo: Int, nextTo: Int): Boolean =
+    prevTo >= nextTo
 
 }
-
--- src/test/scala/algorithms/merge_intervals/MergeOverlappingIntervalsSpec.scala
+++ src/test/scala/algorithms/merge_intervals/MergeOverlappingIntervalsSpec.scala
@@ -7,13 +7,13 @@ class MergeOverlappingIntervalsSpec extends FunSuite {
   test("case 1") {
     val actual = MergeOverlappingIntervals.merge(Seq((1, 5), (3, 7), (4, 6), (6, 8)))
     val expected = Seq((1, 8))
-    assert(expected == actual)
+    assert(expected === actual)
   }
 
   test("case 2") {
     val actual = MergeOverlappingIntervals.merge(Seq((10, 12), (12, 15)))
     val expected = Seq((10, 15))
-    assert(expected == actual)
+    assert(expected === actual)
   }
 
 }
