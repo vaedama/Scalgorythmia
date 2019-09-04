c02acdfe68f9ed49882bbb178d793aeb29e1db81
Venkat Sudheer Reddy Aedama <sudheer.univ@gmail.com>
2019-09-03 22:44:52 -0700

    add docs; minor cleanup

--- src/main/scala/algorithms/merge_intervals/MergeOverlappingIntervals.scala
+++ src/main/scala/algorithms/merge_intervals/MergeOverlappingIntervals.scala
@@ -2,22 +2,23 @@ package algorithms.merge_intervals
 
 import scala.collection.mutable.ArrayBuffer
 
-// Intuition: When does an overlap occur?
-// Given two intervals (prevFrom, prevTo) and (nextFrom, nextTo)
-// an overlap occurs if nextFrom <= prevTo
+/*
+Intuition:
+=========
+When does an overlap occur?
 
-// There are two possible overlapping scenarios:
+Given two intervals (prevFrom, prevTo) and (nextFrom, nextTo), an overlap occurs if nextFrom <= prevTo
+There are two possible overlapping scenarios:
 
-// 1. Full overlap: nextFrom <= prevTo and nextTo <= prevTo
-// Merged = (prevFrom, prevTo)
-// [(1,7), (4,6)] => (1,7)
+1. Full overlap: nextFrom <= prevTo and nextTo <= prevTo ..Merged = (prevFrom, prevTo)
+Example: [(1,7), (4,6)] => (1,7)
 
-// 2. Partial overlap: nextTo > prevTo
-// Merged = (prevFrom, nextTo)
-// [(1,5), (3,7)] => (1,7)
+2. Partial overlap: nextFrom <= prevTo and nextTo > prevTo ...Merged = (prevFrom, nextTo)
+Example: [(1,5), (3,7)] => (1,7)
+ */
 object MergeOverlappingIntervals {
 
-  def merge(intervals: Seq[(Int, Int)]): Seq[(Int, Int)] = {
+  def merge(intervals: Seq[(Int, Int)]): Seq[(Int, Int)] =
     if (intervals.isEmpty) Nil
     else {
       val sorted = intervals.sortBy(_._1)
@@ -36,12 +37,9 @@ object MergeOverlappingIntervals {
       }
       result
     }
-  }
 
-  private def isOverlap(prevTo: Int, nextFrom: Int): Boolean =
-    nextFrom <= prevTo
+  private def isOverlap(prevTo: Int, nextFrom: Int): Boolean = nextFrom <= prevTo
 
-  private def isFullOverlap(prevTo: Int, nextTo: Int): Boolean =
-    prevTo >= nextTo
+  private def isFullOverlap(prevTo: Int, nextTo: Int): Boolean = prevTo >= nextTo
 
 }
