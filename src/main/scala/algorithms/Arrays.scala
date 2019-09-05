33e1fcdca1691438a3fb2695ac0de8869d3acdf9
Venkat Sudheer Reddy Aedama <sudheer.univ@gmail.com>
2019-09-04 19:43:53 -0700

    All Arrays algos accept Vector; improve docs and related cleanup

--- src/main/scala/algorithms/Arrays.scala
+++ src/main/scala/algorithms/Arrays.scala
@@ -1,73 +1,103 @@
 package algorithms
 
 import scala.annotation.tailrec
-import scala.reflect.ClassTag
 
 object Arrays {
 
-  // O(n) time and space complexity
-  // Intuition: Use hash set to look for complement occurrence.
-  // If complement exists, add the pair to the pairs list.
-  // Otherwise, add the occurrence to the occurrence set.
-  def filterPairsAddingUptoN[A](arr: Array[A], n: A)(implicit num: Numeric[A]): List[(A, A)] = {
-    @tailrec def loop(curIdx: Int, occurrences: Set[A] = Set.empty[A], memo: List[(A, A)] = Nil): List[(A, A)] =
-      if (arr.length == curIdx) memo
+  /*
+  Intuition:
+  ==========
+  Use a Set to lookup a complement occurrence.
+  If the complement exists, add the element and it's complement to the pairs list.
+  Otherwise, add the occurrence to the occurrence set and iterate until the end of array.
+
+  O(n) time and space where "n" is size of the array
+   */
+  def filterPairsAddingUptoN[A](arr: Vector[A], n: A)(implicit num: Numeric[A]): List[(A, A)] = {
+    import num._
+
+    @tailrec def loop(idx: Int, occurrences: Set[A] = Set.empty[A], memo: List[(A, A)] = Nil): List[(A, A)] =
+      if (arr.length == idx) memo
       else {
-        val element = arr(curIdx)
-        val complement = num.minus(n, element)
-        if (occurrences(complement)) loop(curIdx + 1, occurrences, (complement, element) :: memo)
-        else loop(curIdx + 1, occurrences + element, memo)
+        val element = arr(idx)
+        val complement = n - element
+        if (occurrences(complement)) loop(idx + 1, occurrences, (complement, element) :: memo)
+        else loop(idx + 1, occurrences + element, memo)
       }
 
     loop(0)
   }
 
-  // O(n) time and space complexity
-  // Intuition: If the array contains zero value, dividing by 0 will throw ArithmeticException
-  // Avoid that by lazily computing non-zero product of the array and using it if we
-  // encounter zero while traversing the array.
-  def productOfAllElementsExceptItself[A: ClassTag](arr: Array[A])(implicit fract: Fractional[A]): Array[A] = {
+  /*
+  Intuition:
+  ==========
+  Product of all elements except itself is same as product of all elements divided by itself.
+  Handle div-by-zero exception by lazily computing the non-zero product of the array and
+  using it instead of the product for "zero" encounters.
+
+  O(n) time and space where "n" is size of the array
+   */
+  def productOfAllElementsExceptItself[A](arr: Vector[A])(implicit fract: Fractional[A]): Vector[A] = {
     val product: A = arr.product
-    lazy val nonZeroProduct: A = arr.filter(_ != 0).product
-    arr.map(a => if (a != 0) fract.div(product, a) else nonZeroProduct)
+
+    import fract._
+    lazy val nonZeroProduct: A = arr.filter(_ != zero).product
+    arr.map(a => if (a != zero) product / a else nonZeroProduct)
   }
 
-  // O(n) time and space complexity
-  // Intuition: Keep track of max and secondMax
-  // If the next element is larger than max, max becomes this element and secondMax becomes max
-  // Otherwise, if the next element is smaller than max but larger than secondMax, this element
-  // becomes secondMax. Continue the iteration until the end of the array is reached.
-  def secondMax[A: ClassTag](array: Array[A])(implicit num: Numeric[A]): A =
-    array match {
-      case Array() => throw new UnsupportedOperationException("secondMax not supported on empty array")
-      case Array(single) => single
-      case Array(f, s, r@_*) =>
-        @tailrec def loop(max: A, secondMax: A, rem: Array[A]): A = rem match {
-          case Array() => secondMax
-          case Array(head, _*) =>
-            if (num.gteq(head, max)) loop(head, max, rem.tail)
-            else if (num.gteq(max, head) && num.gteq(head, secondMax)) loop(max, head, rem.tail)
-            else loop(max, secondMax, rem.tail)
+  /*
+  Intuition:
+  ==========
+  To compute the secondMax, keep track of max and secondMax. While traversing the array,
+  if the current element is larger than max then max becomes current and secondMax becomes max.
+  If the current element is smaller than max and larger than secondMax, max remains the same and
+  the current element becomes secondMax.
+
+  O(n) time and space where "n" is size of the array
+   */
+  def secondMax[A](arr: Vector[A])(implicit num: Numeric[A]): A =
+    arr match {
+      case Vector() => throw new UnsupportedOperationException("secondMax not supported on empty array")
+      case Vector(single) => single
+      case Vector(f, s, r@_*) =>
+        import num._
+
+        @tailrec def loop(max: A, secondMax: A, rem: Seq[A]): A = rem match {
+          case Seq() => secondMax
+          case Seq(h, t@_*) =>
+            if (h >= max) loop(h, max, t)
+            else if (max >= h && h >= secondMax) loop(max, h, t)
+            else loop(max, secondMax, t)
         }
 
-        if (num.gteq(f, s)) loop(f, s, r.toArray)
-        else loop(s, f, r.toArray)
+        if (f >= s) loop(f, s, r)
+        else loop(s, f, r)
     }
 
-  // O(n) time and space
-  def rotateArray[A](seq: IndexedSeq[A], rotations: Int): IndexedSeq[A] = {
-    seq match {
-      case Seq() => seq
-      case Seq(_) => seq
+  /*
+  Intuition:
+  ==========
+  Rotations can be positive or negative
+  If rotations is positive, rotate the array clockwise:
+  Last element becomes head. This last element is prepended to the remaining array.
+
+  If rotations is negative, rotate the array counter-clockwise:
+  First element becomes last. This first element is appended to the tail array.
+
+  O(n) time and space where "n" is number of rotations
+   */
+  def rotateArray[A](arr: Vector[A], rotations: Int): Vector[A] =
+    arr match {
+      case Vector() => arr
+      case Vector(_) => arr
       case _ =>
-        @tailrec def loop(rem: Int, memo: IndexedSeq[A]): IndexedSeq[A] = {
+        @tailrec def loop(rem: Int, memo: Vector[A]): Vector[A] = {
           if (rem == 0) memo
           else if (rem > 0) loop(rem - 1, memo.last +: memo.dropRight(1))
           else loop(rem + 1, memo.tail :+ memo.head)
         }
 
-        loop(rotations, seq)
+        loop(rotations, arr)
     }
-  }
 
 }
--- src/test/scala/algorithms/ArraysSpec.scala
+++ src/test/scala/algorithms/ArraysSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class ArraysSpec extends FunSuite {
 
-  val arr = Array(1, 21, 3, 14, 5, 60, 7, 6)
+  val arr = Vector(1, 21, 3, 14, 5, 60, 7, 6)
 
   test("filterPairsAddingUptoN: single match") {
     val actual = Arrays.filterPairsAddingUptoN(arr, 28)
@@ -24,34 +24,34 @@ class ArraysSpec extends FunSuite {
   }
 
   test("productOfAllElementsExceptItself: given an array with non-zero elements") {
-    val nonZeroArr = Array(-1.0, 2, -3, 4, -5)
+    val nonZeroArr = Vector(-1.0, 2, -3, 4, -5)
     val actual = Arrays.productOfAllElementsExceptItself(nonZeroArr)
     val expected = Array(120, -60, 40, -30, 24)
     assert(expected === actual)
   }
 
   test("productOfAllElementsExceptItself: given an array with a zero element") {
-    val nonZeroArr = Array(-1.0, 2, -3, 4, 0, -5)
+    val nonZeroArr = Vector(-1.0, 2, -3, 4, 0, -5)
     val actual = Arrays.productOfAllElementsExceptItself(nonZeroArr)
     val expected = Array(0, 0, 0, 0, -120, 0)
     assert(expected === actual)
   }
 
   test("secondMax") {
-    val arr = Array(-2, -33, -10, -456)
+    val arr = Vector(-2, -33, -10, -456)
     val actual = Arrays.secondMax(arr)
     val expected = -10
     assert(expected === actual)
   }
 
   test("rotateArray: negative rotations") {
-    val actual = Arrays.rotateArray(IndexedSeq(1, 10, 20, 0, 59, 86, 32, 11, 9, 40), -1)
+    val actual = Arrays.rotateArray(Vector(1, 10, 20, 0, 59, 86, 32, 11, 9, 40), -1)
     val expected = IndexedSeq(10, 20, 0, 59, 86, 32, 11, 9, 40, 1)
     assert(expected === actual)
   }
 
   test("rotateArray: positive rotations") {
-    val actual = Arrays.rotateArray(IndexedSeq(1, 10, 20, 0, 59, 86, 32, 11, 9, 40), 2)
+    val actual = Arrays.rotateArray(Vector(1, 10, 20, 0, 59, 86, 32, 11, 9, 40), 2)
     val expected = IndexedSeq(9, 40, 1, 10, 20, 0, 59, 86, 32, 11)
     assert(expected === actual)
   }
